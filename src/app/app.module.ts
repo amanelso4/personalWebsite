dd475e7b7cfd00e6f4e6195e39deaf23f7a61912
KT43 <amanda.x.nelson@sunlife.com>
2019-08-05 15:20:11 -0500

    Initialize Contact Me Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+
+
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { JobHistoryComponent } from './job-history/job-history.component';
